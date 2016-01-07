8009820603ce900fcc27679787e7d14f59d852b6
KJ Park <kumjip@KJs-MacBook-Pro.local>
2016-01-06 22:16:52 -0800

    modified 1_8

--- 1_8.js
+++ 1_8.js
@@ -1,4 +1,9 @@
-var isSubString = function(s1, s2) {
+//Assumed you have a method isSubstring which checks if one word is a substring of another.
+//Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
+//e.g., "waterbottle" is a rotation of "erbottlewat"
+
+
+var isSubstring = function(s1, s2) {
 	if(s1.length == s2.length) {
 		var s1s1 = s1+s1;
 		console.log(s1s1.indexOf(s2));
@@ -13,7 +18,7 @@ var isSubString = function(s1, s2) {
 var s1 = 'waterbottle';
 var s2 = 'erbottlewat';
 
-if(isSubString(s1,s2))
+if(isSubstring(s1,s2))
 	console.log('It is rotated.');
 else
 	console.log('It is not rotated.');
